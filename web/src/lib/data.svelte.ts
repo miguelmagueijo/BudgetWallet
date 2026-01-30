05fb05e828ecbd00eb59f69e637c10d1c242e568
Miguel Magueijo <miguelmagueijo@hotmail.com>
2026-01-30 22:45:23 +0000

    web: add sort to DataStore class

--- web/src/lib/data.svelte.ts
+++ web/src/lib/data.svelte.ts
@@ -1,16 +1,37 @@
 export class DataStore<T> {
 	private filterFunction: ((element: T) => boolean) | null = $state(null);
+	private sortFunction: ((a: T, b: T) => number) | null = $state(null);
 	private originalData: Array<T> = $state([]);
 
 	public loading: boolean = $state(false);
 	public dataOut: Array<T> = $derived.by(() => {
-		if (!this.filterFunction) return this.originalData;
+		let output = this.originalData.slice();
 
-		return this.originalData.filter(this.filterFunction);
+		if (output.length === 0) {
+			return output;
+		}
+
+		if (this.filterFunction) {
+			output = output.filter(this.filterFunction);
+		}
+
+		if (this.sortFunction) {
+			output = output.sort(this.sortFunction);
+		}
+
+		return output;
 	});
 
 	public setData(data: Array<T>) {
 		this.originalData = data;
+	}
+
+	public applySort(sortFunction: (a: T, b: T) => number) {
+		this.sortFunction = sortFunction;
+	}
+
+	public resetSort() {
+		this.sortFunction = null;
 	}
 
 	public applyFilter(filterFunction: (element: T) => boolean) {
