14ef1e784b4058cdc824c26573c20ed7d7ef8dac
Miguel Magueijo <miguelmagueijo@hotmail.com>
2026-01-31 01:15:08 +0000

    api: add new objects for two tables, incomplete for movements

--- restapi/db_models.py
+++ restapi/db_models.py
@@ -28,7 +28,33 @@ class DbWallet(BaseDbModel, table=True):
     name: str = Field(nullable=False)
     description: str = Field(nullable=True)
     iconify_name: str = Field(nullable=False)
-    color: str = Field(nullable=False)
+    color: str = Field(nullable=True, regex=r"^#[0-9a-fA-F]{6}$")
     start_balance: float = Field(default=0, lt=1000000, gt=-1000000)
     user_id: int = Field(nullable=False, foreign_key="user_account.id")
     user: DbUser = Relationship(back_populates="wallets")
+
+    budgets: list["DbBudget"] = Relationship(back_populates="wallet")
+
+class DbBudget(BaseDbModel, table=True):
+    __tablename__ = "budget"
+
+    name: str = Field(nullable=False)
+    description: str = Field(nullable=True)
+    iconify_name: str = Field(nullable=True)
+    color: str = Field(nullable=True, regex=r"^#[0-9a-fA-F]{6}$")
+    is_permanent: bool = Field(nullable=False, default=False)
+
+    wallet_id: int = Field(nullable=False, foreign_key="wallet.id")
+    wallet: DbWallet = Relationship(back_populates="budgets")
+
+    movements: list["DbMovement"] = Relationship(back_populates="budget")
+
+class DbMovement(BaseDbModel, table=True):
+    __tablename__ = "movement"
+
+    title: str = Field(nullable=False)
+    amount: float = Field(nullable=False)
+    is_deposit: bool = Field(nullable=False)
+
+    budget_id: int = Field(nullable=False, foreign_key="budget.id")
+    budget: DbBudget = Relationship(back_populates="movements")
