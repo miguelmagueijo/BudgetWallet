7f3d9f271204097c5ec61f94616087fdd2803036
Miguel Magueijo <miguelmagueijo@hotmail.com>
2026-02-05 23:27:36 +0000

    web: add ICON_NAMES to lib index

--- web/src/lib/index.ts
+++ web/src/lib/index.ts
@@ -1,1 +1,6 @@
-// place files you want to import through the `$lib` alias in this folder.
+export const ICONS_NAMES = {
+	accountSettings: "mdi:account-cog",
+	roundInfo: "akar-icons:info-fill",
+	trash: "weui:delete-filled",
+	logout: "ic:round-logout",
+} as const;
