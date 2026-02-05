70732f7e8513f1704baddc2bb97ecf8fc7dad8b9
Miguel Magueijo <miguelmagueijo@hotmail.com>
2026-02-05 23:28:36 +0000

    web: add roadmap page with no content

---
+++ web/src/routes/(with-auth)/roadmap/+page.svelte
@@ -0,0 +1,6 @@
+<script>
+	import { resolve } from "$app/paths";
+</script>
+
+<h1 class="mt-10 text-center text-4xl font-bold">Coming soon...</h1>
+<a class="primary-button-outline mx-auto mt-4 block w-fit px-4 py-2 text-sm" href={resolve("/home")}>Return home</a>
