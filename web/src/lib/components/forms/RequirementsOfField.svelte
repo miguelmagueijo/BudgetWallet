a80dd840a96e5f74db6ba6a6e08826a40420623d
Miguel Magueijo <miguelmagueijo@hotmail.com>
2026-02-05 22:45:06 +0000

    web: create RequirementsOfField component to show requirements of form fields

---
+++ web/src/lib/components/forms/RequirementsOfField.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+	import Icon from "@iconify/svelte";
+
+	interface Props {
+		requirements: Array<string>;
+	}
+
+	const { requirements }: Props = $props();
+</script>
+
+<div class="group relative mt-1 cursor-help">
+	<div class="flex items-center gap-1 opacity-50 select-none">
+		<p class="text-sm">Requirements</p>
+		<Icon icon="akar-icons:info-fill" class="size-4" />
+	</div>
+	<div class="absolute top-full left-0 z-10 hidden rounded-lg border-2 border-gray-600 bg-black p-2 text-sm select-none group-hover:block">
+		<p class="text-xs opacity-50">Field requirements</p>
+		{#if requirements.length === 1}
+			<p>{requirements[0]}</p>
+		{:else}
+			<ul class="list-inside list-disc text-gray-200">
+				{#each requirements as req, idx (idx)}
+					<li>{req}</li>
+				{/each}
+			</ul>
+		{/if}
+	</div>
+</div>
