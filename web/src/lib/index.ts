211ea0431f9c8b2d32b299f8bf8fa06ca7d1c2f9
Miguel Magueijo <miguelmagueijo@hotmail.com>
2026-02-06 22:32:39 +0000

    web: add new icons for toasts

--- web/src/lib/index.ts
+++ web/src/lib/index.ts
@@ -1,6 +1,9 @@
 export const ICONS_NAMES = {
 	accountSettings: "mdi:account-cog",
-	roundInfo: "akar-icons:info-fill",
 	trash: "weui:delete-filled",
 	logout: "ic:round-logout",
+	roundError: "fluent:error-circle-12-filled",
+	roundWarning: "fluent:warning-12-filled",
+	roundCheck: "fluent:checkmark-circle-12-filled",
+	roundInfo: "fluent:info-12-filled",
 } as const;
