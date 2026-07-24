6873692a6f926542cc872287c59ce018847700a3
Miguel Magueijo <miguelmagueijo@hotmail.com>
2026-07-24 22:21:49 +0100

    api: update db_utils.py parameters types

--- restapi/db_utils.py
+++ restapi/db_utils.py
@@ -2,19 +2,19 @@ from typing import Any
 
 from fastapi import HTTPException
 from pydantic import BaseModel
+from sqlmodel import Session
 
 from db_models import BaseDbModel
-from dependencies import DbSessionDependency
 
 
-def generic_record_delete(db_session: DbSessionDependency, target: BaseDbModel | None, not_existing_msg: str):
+def generic_record_delete(db_session: Session, target: BaseDbModel | None, not_existing_msg: str):
     if target is None:
         raise HTTPException(status_code=404, detail=not_existing_msg)
 
     db_session.delete(target)
     db_session.commit()
 
-def generic_record_patch(db_session: DbSessionDependency, target: BaseDbModel | None, data: BaseModel,
+def generic_record_patch(db_session: Session, target: BaseDbModel | None, data: BaseModel,
                           not_existing_msg: str):
     if target is None:
         raise HTTPException(status_code=404, detail=not_existing_msg)
