fb80c2769719b9e6ab0aea724ef2bcf4a8576ce6
Miguel Magueijo <miguelmagueijo@hotmail.com>
2026-07-26 11:24:25 +0100

    web: add wallets page

---
+++ web/src/routes/(with-auth)/wallets/+page.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+	import Icon from "@iconify/svelte";
+	import { onMount } from "svelte";
+
+	let data = $state({});
+
+	onMount(async () => {
+		fetch("/api/wallets/", {
+			method: "GET",
+		});
+	});
+</script>
+
+<section>
+	<a href="/home" class="mb-4 flex w-fit items-center gap-2 underline opacity-50">
+		<Icon icon="pajamas:go-back" />
+		<span>Home</span>
+	</a>
+	<h1 class="text-5xl font-bold">My wallets</h1>
+</section>
