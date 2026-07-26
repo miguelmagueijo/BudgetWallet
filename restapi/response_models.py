aecb5b4431b497c2643d46c2abea4029d3fe81c5
Miguel Magueijo <miguelmagueijo@hotmail.com>
2026-07-26 12:01:12 +0100

    api: add response_models.py with array and object models (new response types)

---
+++ restapi/response_models.py
@@ -0,0 +1,13 @@
+from typing import Any, TypeVar, Generic
+
+from pydantic import BaseModel
+
+T = TypeVar("T")
+
+class ArrayDataResponseModel(BaseModel, Generic[T]):
+    data: list[T]
+    meta: dict[str, T] | None = None
+
+class ObjectDataResponseModel(BaseModel, Generic[T]):
+    data: T
+    meta: dict[str, T] | None = None
