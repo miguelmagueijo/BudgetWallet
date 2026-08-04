426f9a1e132fbeaaef2373a736b7509ed3e218f1
Miguel Magueijo <miguelmagueijo@hotmail.com>
2026-08-04 20:56:37 +0100

    web: improvements to wallet drawer to use object, show loading cards when loading movements, allow to edit wallets (not yet working to backend)

--- web/src/routes/(with-auth)/wallets/+page.svelte
+++ web/src/routes/(with-auth)/wallets/+page.svelte
@@ -45,6 +45,14 @@
 		budgetBalance: number;
 	}
 
+	interface WalletDrawerData {
+		isOpen: boolean;
+		editId: number | null;
+		name: string;
+		icon?: string;
+		color?: string;
+	}
+
 	const walletsStore: DataStore<IWallet> = new DataStore<IWallet>("/api/wallets/?with_budgets=true");
 
 	let movementsStore: DataStore<IMovement> | null = $state(null);
@@ -76,40 +84,71 @@
 		showBudgetDetails = false;
 	}
 
-	let showDetailsWalletDrawer = $state(true);
-	let walletDtlsName = $state("");
-	let walletDtlsIcon = $state("");
-	let walletDtlsColor = $state("#FFFFFF");
+	const walletDrawerData: WalletDrawerData = $state({
+		isOpen: false,
+		editId: null,
+		name: "",
+		icon: "",
+		color: "#FFFFFF"
+	});
+
+	function openWalletDrawer(walletToEdit?: IWallet) {
+		if (!walletToEdit) {
+			walletDrawerData.editId = null;
+			walletDrawerData.name = "";
+			walletDrawerData.icon = "";
+			walletDrawerData.color = "#FFFFFF";
+		} else {
+			walletDrawerData.editId = walletToEdit.id;
+			walletDrawerData.name = walletToEdit.name;
+			walletDrawerData.icon = walletToEdit.icon;
+			walletDrawerData.color = walletToEdit.color ?? "#FFFFFF";
+		}
+
+		walletDrawerData.isOpen = true;
+	}
 </script>
 
 <svelte:head>
 	<title>BW | Wallets</title>
 </svelte:head>
 
-<Drawer bind:showDrawer={showDetailsWalletDrawer}>
+<Drawer bind:showDrawer={walletDrawerData.isOpen}>
 	{#snippet header()}
 		<div class="flex items-center justify-between">
-			<h2 class="text-xl font-bold">New wallet</h2>
-			<button class="cursor-pointer text-gray-600" onclick={() => (showDetailsWalletDrawer = false)}>
+			<h2 class="text-xl font-bold">
+				{#if walletDrawerData.editId === null}
+					New wallet
+				{:else}
+					Edit wallet
+				{/if}
+			</h2>
+			<button class="cursor-pointer text-gray-600" onclick={() => (walletDrawerData.isOpen = false)}>
 				<Icon icon={ICONS_NAMES.x} class="size-5" />
 			</button>
 		</div>
-		<p class="mt-2 text-gray-500">Create a new tracking of your money</p>
+		<p class="mt-2 text-gray-500">
+			{#if walletDrawerData.editId === null}
+				Create a new tracking of your money
+			{:else}
+				Let's update the details
+			{/if}
+		</p>
 	{/snippet}
 	<form>
 		<label class="block font-semibold">
 			Name <span class="text-red-500">*</span>
-			<input type="text" class="mt-2 w-full rounded-lg border-2 border-primary-800 bg-primary-1000" bind:value={walletDtlsName} />
+			<input type="text" class="mt-2 w-full rounded-lg border-2 border-primary-800 bg-primary-1000" bind:value={walletDrawerData.name} />
 		</label>
-		<label class="mt-8 block font-semibold">
+		<label class="mt-6 block font-semibold">
 			Color
-			<input type="color" class="mt-2 h-11 w-full rounded-lg border-2 border-transparent" bind:value={walletDtlsColor} />
+			<input type="color" class="mt-2 h-11 w-full rounded-lg border-2 border-transparent" bind:value={walletDrawerData.color} />
 			<small class="text-xs text-gray-500">Defaults to white</small>
 		</label>
-		<div class="mt-8 items-center justify-between gap-4 {walletDtlsIcon ? 'grid grid-cols-[1fr_auto]' : ''}">
+		<div class="mt-6 items-center justify-between gap-4 {walletDrawerData.icon ? 'grid grid-cols-[1fr_auto]' : ''}">
 			<label class="block font-semibold">
 				Icon
-				<input type="text" class="mt-2 h-11 w-full rounded-lg border-2 border-primary-800 bg-primary-1000" bind:value={walletDtlsIcon} />
+				<input type="text" class="mt-2 h-11 w-full rounded-lg border-2 border-primary-800 bg-primary-1000" bind:value={walletDrawerData.icon} />
 				<small class="text-xs text-gray-500">
 					<a class="mt-2 flex items-center gap-1 underline" href="https://icon-sets.iconify.design/" target="_blank">
 						<span>Open icons catalog</span>
@@ -117,13 +156,13 @@
 					</a>
 				</small>
 			</label>
-			{#if walletDtlsIcon}
+			{#if walletDrawerData.icon}
 				<div
 					class="flex size-18 items-center justify-center rounded-lg border-2 border-primary-800 p-3"
-					style:color={walletDtlsColor}
-					style:border-color={walletDtlsColor}
+					style:color={walletDrawerData.color}
+					style:border-color={walletDrawerData.color}
 				>
-					<Icon icon={walletDtlsIcon} class="size-full" />
+					<Icon icon={walletDrawerData.icon} class="size-full stroke-2" />
 				</div>
 			{/if}
 		</div>
@@ -132,13 +171,17 @@
 		<div class="flex gap-2">
 			<button
 				class="w-26 cursor-pointer rounded-lg border-2 border-transparent bg-primary-600 py-2 font-semibold text-primary-0 duration-200 hover:bg-primary-800"
-				onclick={() => (showDetailsWalletDrawer = false)}
+				onclick={() => (walletDrawerData.isOpen = false)}
 			>
-				Create
+				{#if walletDrawerData.editId === null}
+					Create
+				{:else}
+					Update
+				{/if}
 			</button>
 			<button
 				class="w-26 cursor-pointer rounded-lg border-2 border-transparent py-2 font-semibold duration-200 hover:bg-gray-800"
-				onclick={() => (showDetailsWalletDrawer = false)}
+				onclick={() => (walletDrawerData.isOpen = false)}
 			>
 				Cancel
 			</button>
@@ -183,34 +226,47 @@
 					<div class="flex flex-col gap-y-2 rounded-lg bg-primary-1000 p-4">
 						<div class="font-bold">Movements</div>
 						{#if movementsStore}
-							{#each movementsStore.dataOut as movement (movement.id)}
-								{@const realAmount = Number(Number(movement.amount).toFixed(2)) * (movement.is_deposit ? 1 : -1)}
-								{@const realDoneDate = new Date(movement.done_at).toLocaleDateString("pt-PT", { hour: "2-digit", minute: "2-digit" })}
-								<div class="rounded-lg border-2 border-primary-0/15 p-3">
-									<div class="flex items-center justify-between">
-										<div class="text-sm opacity-50">
-											{realDoneDate}
-										</div>
-										{#if movement.category}
-											<div
-												class="w-fit rounded-full px-4 text-xs font-semibold"
-												style:background-color={movement.category.color}
-												style:color={getTextColorForHexBg(movement.category.color)}
-											>
-												{movement.category.title}
+							{#if movementsStore.loading}
+								{#each [1, 2] as i (i)}
+									<div class="rounded-lg border-2 border-primary-0/15 p-3 animate-pulse">
+										<div class="h-3 bg-primary-0/15 rounded-lg mb-2"></div>
+										<div class="h-5 bg-primary-0/15 rounded-lg"></div>
+									</div>
+								{/each}
+							{:else}
+								{#each movementsStore.dataOut as movement (movement.id)}
+									{@const realAmount = Number(Number(movement.amount).toFixed(2)) * (movement.is_deposit ? 1 : -1)}
+									{@const realDoneDate = new Date(movement.done_at).toLocaleDateString("pt-PT", { hour: "2-digit", minute: "2-digit" })}
+									<div class="rounded-lg border-2 border-primary-0/15 p-3">
+										<div class="flex items-center justify-between">
+											<div class="text-sm opacity-50">
+												{realDoneDate}
 											</div>
-										{/if}
-									</div>
-									<div class="flex items-center justify-between">
-										<div>
-											{movement.title}
+											{#if movement.category}
+												<div
+													class="w-fit rounded-full px-4 text-xs font-semibold"
+													style:background-color={movement.category.color}
+													style:color={getTextColorForHexBg(movement.category.color)}
+												>
+													{movement.category.title}
+												</div>
+											{/if}
 										</div>
-										<div class="font-bold" class:text-primary-500={movement.is_deposit} class:text-red-500={!movement.is_deposit}>
-											<b>{realAmount}</b> <small>€</small>
+										<div class="flex items-center justify-between">
+											<div>
+												{movement.title}
+											</div>
+											<div class="font-bold" class:text-primary-500={movement.is_deposit} class:text-red-500={!movement.is_deposit}>
+												<b>{realAmount}</b> <small>€</small>
+											</div>
 										</div>
 									</div>
-								</div>
-							{/each}
+								{:else}
+									<p class="text-gray-500">
+										No movements yet registered
+									</p>
+								{/each}
+							{/if}
 						{:else}
 							Something went wrong!
 						{/if}
@@ -240,7 +296,7 @@
 	<div class="mt-12">
 		<button
 			class="flex cursor-pointer items-center gap-2 rounded-lg bg-primary-600 px-4 py-2 font-semibold"
-			onclick={() => (showDetailsWalletDrawer = true)}
+			onclick={() => openWalletDrawer()}
 		>
 			<Icon icon={ICONS_NAMES.plus} class="size-4" />
 			New wallet
@@ -266,7 +322,7 @@
 		<div class="mt-12 flex flex-col gap-6">
 			{#each walletsStore.dataOut as wallet (wallet.id)}
 				{@const walletBalance = Number(Number(wallet.balance).toFixed(2))}
-				<div class="rounded-lg border-2 border-primary-0/25 bg-primary-950 p-4" style:border-color={wallet.color}>
+				<div class="rounded-lg border-2 border-primary-0/25 bg-primary-950 p-4 group" style:border-color={wallet.color}>
 					<div class="flex items-center justify-between">
 						<div class="flex items-center gap-2" style:color={wallet.color}>
 							{#if wallet.icon}
@@ -275,6 +331,9 @@
 							<h2 class="text-xl font-bold">
 								{wallet.name}
 							</h2>
+							<button class="p-1 rounded-lg border-2 border-primary-0/50 text-primary-0/50 hover:bg-primary-700 duration-300 cursor-pointer hover:border-primary-0 hover:text-primary-0 hidden group-hover:block" onclick={() => openWalletDrawer(wallet)}>
+								<Icon icon={ICONS_NAMES.edit}/>
+							</button>
 						</div>
 						<div class="text-xl">
 							<b class:text-primary-500={walletBalance > 0}>{walletBalance}</b> <span class="text-sm">€</span>
@@ -300,7 +359,7 @@
 								<div class="p-4 bg-primary-1000 rounded-lg text-primary-0/35">No budgets</div>
 							{/each}
 						{:else}
-							sadsda
+							<div class="p-4 bg-primary-1000 rounded-lg text-red-500">Erro fetching budgets</div>
 						{/if}
 					</div>
 				</div>
